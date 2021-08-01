51a186d84b148be6163ee31d0154a132812819dc
Louys Hong <louyshong1998@gmail.com>
2021-08-01 13:36:08 +0100

    Correct form fields in second page

--- public/main.js
+++ public/main.js
@@ -35,10 +35,43 @@ eduAddButton.addEventListener("click", function() {
     document.querySelector(".education").insertBefore(newField, document.querySelector(".education .add-btn"));
 })
 
+empAddButton.addEventListener("click", function() {
+    // Clone set of entries
+    const newField = document.querySelector(".employment .entry").cloneNode(true);
+
+    // Reset inputs 
+    const inputs = newField.getElementsByTagName("input");
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].value = null;
+    }
+
+    // Insert horizontal line 
+    const line = document.createElement("hr")
+    document.querySelector(".employment").insertBefore(line, document.querySelector(".employment .add-btn"));
+    
+    // Insert new set of entries
+    document.querySelector(".employment").insertBefore(newField, document.querySelector(".employment .add-btn"));
+})
+
 // Remove form fields on second page 
 eduRemoveButton.addEventListener("click", function() {
     const allDivs = document.querySelectorAll(".education .entry");
     const allLines = document.querySelectorAll(".education hr");
+
+    // Remove horizontal line
+    if (allLines.length > 0) {
+        allLines[allLines.length - 1].remove();
+    }
+
+    // Remove the last div 
+    if (allDivs.length > 1) {
+        allDivs[allDivs.length - 1].remove();
+    }
+})
+
+empRemoveButton.addEventListener("click", function() {
+    const allDivs = document.querySelectorAll(".employment .entry");
+    const allLines = document.querySelectorAll(".employment hr");
 
     // Remove horizontal line
     if (allLines.length > 0) {
