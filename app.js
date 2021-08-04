99a8dfcfe4f8dfc18695dbce3140d097cfec9f7b
Louys Hong <louyshong1998@gmail.com>
2021-08-04 12:57:52 +0100

    Limit upload file size to 1MB

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const {google} = require("googleapis");
 require('dotenv').config();
 
 const app = express();
-const upload = multer();
+const upload = multer({limits: {fileSize: 1024 * 1024}}).single("CV"); // Maximum file upload size is 1MB
 const OAuth2 = google.auth.OAuth2;
 
 // Configure recipient email and service
@@ -49,89 +49,96 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/form.html");
 });
 
-app.post("/", upload.single('CV'), function(req, res) {
-    let name = req.body.Name; 
-    let contact = req.body.Contact;
-    let address = req.body.Address;
-    let position = req.body.Position;
-    let schools = req.body.School; 
-    let programmes = req.body.Programme; 
-    let grades = req.body.Grade;
-    let titles = req.body.Title; 
-    let companies = req.body.Company;
-    let locations = req.body.Location;
-    let startDates = req.body.StartDate; 
-    let endDates = req.body.EndDate; 
-
-    // Build message text
-    let text = "***** Applicant Details *****" + "\n";
-    
-    text = text + "Applicant name: " + name + "\n" +
-           "Contact: " + contact + "\n" +
-           "Address: " + address + "\n" + 
-           "Position applied for: " + position + "\n" + "\n";
-
-    // Ensure that these are always arrays (to loop through in for loop)
-    if (!Array.isArray(schools)) {
-        schools = [schools];
-        programmes = [programmes];
-        grades = [grades];
-    }
-
-    // Ensure that these are always arrays (to loop through in for loop)
-    if (!Array.isArray(titles)) {
-        titles = [titles];
-        companies = [companies];
-        locations = [locations];
-        startDates = [startDates];
-        endDates = [endDates];
-    }
-    
-    text = text + "***** Education *****" + "\n";
-
-    for (let i = 0; i < schools.length; i++) {
-        // These are all same length as they are required fields 
-        let newEntry = "School: " + schools[i] + "\n" + 
-                       "Programme: " + programmes[i] + "\n" + 
-                       "Grade: " + grades[i] + "\n"; 
-
-        text = text + newEntry + "\n"; 
-    }
-
-    text = text + "***** Employment History *****" + "\n";
-
-    for (let i = 0; i < titles.length; i++) {
-        // These are all same length as they are required fields
-        let newEntry = "Title: " + titles[i] + "\n" + 
-                       "Company: " + companies[i] + "\n" + 
-                       "Location: " + locations[i] + "\n" + 
-                       "Start date: " + startDates[i] + "\n" + 
-                       "End date: " + endDates[i] + "\n";
-        
-        text = text + newEntry + "\n";
-    }
-
-    // console.log(text);
-    // console.log(req);
-    // console.log(req.file);
-
-    // Construct message
-    let message = {
-        from: serverEmail,
-        to: recipientEmail,
-        subject: "New application from " + name + "!",
-        attachments: [{filename: name + "_CV.pdf", content: req.file.buffer}],
-        text: text
-    };
-
-    // Send mail 
-    transporter.sendMail(message, function(error, info) {
-        if (error) {
-            console.log("Error: ", error);
+app.post("/", function(req, res) {
+    upload(req, res, function(err) {
+        if (err) {
+            console.log("Error: ", err);
             res.redirect("/failure");
         } else {
-            console.log("Email sent: " + info.response);
-            res.redirect("/success");
+            let name = req.body.Name; 
+            let contact = req.body.Contact;
+            let address = req.body.Address;
+            let position = req.body.Position;
+            let schools = req.body.School; 
+            let programmes = req.body.Programme; 
+            let grades = req.body.Grade;
+            let titles = req.body.Title; 
+            let companies = req.body.Company;
+            let locations = req.body.Location;
+            let startDates = req.body.StartDate; 
+            let endDates = req.body.EndDate; 
+
+            // Build message text
+            let text = "***** Applicant Details *****" + "\n";
+            
+            text = text + "Applicant name: " + name + "\n" +
+                "Contact: " + contact + "\n" +
+                "Address: " + address + "\n" + 
+                "Position applied for: " + position + "\n" + "\n";
+
+            // Ensure that these are always arrays (to loop through in for loop)
+            if (!Array.isArray(schools)) {
+                schools = [schools];
+                programmes = [programmes];
+                grades = [grades];
+            }
+
+            // Ensure that these are always arrays (to loop through in for loop)
+            if (!Array.isArray(titles)) {
+                titles = [titles];
+                companies = [companies];
+                locations = [locations];
+                startDates = [startDates];
+                endDates = [endDates];
+            }
+            
+            text = text + "***** Education *****" + "\n";
+
+            for (let i = 0; i < schools.length; i++) {
+                // These are all same length as they are required fields 
+                let newEntry = "School: " + schools[i] + "\n" + 
+                            "Programme: " + programmes[i] + "\n" + 
+                            "Grade: " + grades[i] + "\n"; 
+
+                text = text + newEntry + "\n"; 
+            }
+
+            text = text + "***** Employment History *****" + "\n";
+
+            for (let i = 0; i < titles.length; i++) {
+                // These are all same length as they are required fields
+                let newEntry = "Title: " + titles[i] + "\n" + 
+                            "Company: " + companies[i] + "\n" + 
+                            "Location: " + locations[i] + "\n" + 
+                            "Start date: " + startDates[i] + "\n" + 
+                            "End date: " + endDates[i] + "\n";
+                
+                text = text + newEntry + "\n";
+            }
+
+            // console.log(text);
+            // console.log(req);
+            // console.log(req.file);
+
+            // Construct message
+            let message = {
+                from: serverEmail,
+                to: recipientEmail,
+                subject: "New application from " + name + "!",
+                attachments: [{filename: name + "_CV.pdf", content: req.file.buffer}],
+                text: text
+            };
+
+            // Send mail 
+            transporter.sendMail(message, function(error, info) {
+                if (error) {
+                    console.log("Error: ", error);
+                    res.redirect("/failure");
+                } else {
+                    console.log("Email sent: " + info.response);
+                    res.redirect("/success");
+                }
+            })
         }
     })
 });
